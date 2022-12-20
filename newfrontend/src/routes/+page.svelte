82c05686309498fb7452c444118162af9b817d0d
Edward C <76141700+edwardwc@users.noreply.github.com>
2022-12-20 18:44:14 -0500

    hello world

--- newfrontend/src/routes/+page.svelte
+++ newfrontend/src/routes/+page.svelte
@@ -2,10 +2,8 @@
     import {onMount, } from "svelte";
     import { fade } from "svelte/transition";
 
-
-    let loading = true
     let message = "Loading..."
-    let currentlySelected = "";
+    let currentlySelected = "dark";
 
     onMount(async () => {
         const logElem = document.querySelector('#log')
@@ -13,7 +11,6 @@
         const formElem = document.querySelector('#chatform')
         const inputElem = document.querySelector('#text')
         const clientsElem = document.querySelector('#clients')
-        const allThemes = ["cyberpunk", "luxury", "halloween", "forest", "aqua"]
 
         /** @type {WebSocket | null} */
         var socket = null
@@ -22,14 +19,10 @@
             if (sender === "system") {
                 logElem.innerHTML += `<p class="text-center">${msg}</p>`
             } else if (you) {
-                let number = Math.floor(Math.random() * (allThemes.length))
-                console.log(number)
-                let color = allThemes[number];
-                console.log(color)
                 logElem.innerHTML += `
                 <div class="chat chat-end">
                   ` + (!sentLastMessage.includes("you") ? `<div className="chat-header">${sender}</div>` : ``) + `
-                  <div class="chat-bubble opacity-100 brightness-200" data-theme="${color}">${msg}</div>
+                  <div class="chat-bubble opacity-100 brightness-200" data-theme="${currentlySelected}">${msg}</div>
                 </div>
                 `
                 // will add div here
@@ -38,12 +31,12 @@
                 logElem.innerHTML +=
                     sentLastMessage === sender ? `
                     <div class="chat chat-start max-w-screen-md lg:max-w-4xl pt-0">
-                       <div class="chat-bubble">${msg}</div>
+                       <div class="chat-bubble" data-theme="${currentlySelected}">${msg}</div>
                     </div>
                     ` : `
                 <div class="chat chat-start max-w-screen-md lg:max-w-4xl">
                        <div class="chat-header">${sender}</div>
-                       <div class="chat-bubble" data-theme="">${msg}</div>
+                       <div class="chat-bubble" data-theme="${currentlySelected}">${msg}</div>
                 </div>`
                 sentLastMessage = sender;
             }
@@ -65,15 +58,11 @@
                 const data = JSON.parse(ev.data)
                 if (data.Sender.includes("(you)")) {
                     log(data.Message, data.Sender, true);
-                    console.log(1)
                 } else if (data.Sender === "update") {
                     clientsElem.innerHTML = data.Message
-                    console.log(2)
                 } else {
                     log(data.Message, data.Sender, false)
-                    console.log(3)
                 }
-                console.log(4)
             }
 
             socket.onclose = () => {
@@ -94,7 +83,6 @@
             inputElem.focus()
         })
     })
-    loading = false
 
 </script>
 <style>
@@ -102,9 +90,9 @@
         overflow: auto;
     }
 </style>
-<html data-theme="{currentlySelected}">
+<html>
     <div class={"py-5 text-center"}>
-        <p transition:fade class={"lg:text-8xl md:text-6xl text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-indigo-500" +
+        <p transition:fade class={"lg:text-8xl text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-indigo-500" +
                       " via-purple-500 to-pink-500"}>
             rizzle.chat
         </p>
@@ -119,36 +107,33 @@
             */
         -->
     </div>
-    <div class="relative text-right lg:px-8 px-1.5 py-5">
-        <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn m-1">Style</label>
-            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                <li data-theme="dark" on:click={() => {
-                    currentlySelected = "dark"
-                }}><a>Normal</a></li>
-                <li data-theme="cyberpunk" on:click={() => {
+    <div class="lg:hover:blur-none">
+        <div class={"mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 border-solid border-2 border-sky-500 rounded-lg" +
+         " opacity-90 py-10 bg-gray-800"} style={"height: 50vh;"} id="log">
+        </div>
+        <div>
+            <form id="chatform" class="text-right px-24 py-2" autocomplete="off">
+                <input type="text" class={`input input-bordered w-full max-w-xs m-1 ` + (currentlySelected === "cyberpunk" ? `bg-gray-700 text-yellow-400` : ``)}
+                       data-theme="{currentlySelected}" id="text" autofocus required/>
+                <button type="submit" data-theme="{currentlySelected}" class="btn btn-success px-10" id="send">
+                    Send
+                </button>
+                <div class="dropdown dropdown-top dropdown-end">
+                    <label tabindex="0" class="btn m-1" data-theme="{currentlySelected}">Style</label>
+                    <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li data-theme="cyberpunk" on:click={() => {
                     currentlySelected = "cyberpunk"
                 }}><a>Cyberpunk</a></li>
-                <li data-theme="luxury" on:click={() => {
+                        <li data-theme="dark" on:click={() => {
+                    currentlySelected = "dark"
+                }}><a>Dark</a></li>
+                        <li data-theme="luxury" on:click={() => {
                     currentlySelected = "luxury"
                 }}><a>Luxury</a></li>
-            </ul>
+                    </ul>
+                </div>
+            </form>
         </div>
     </div>
-    {#if loading}
-        <p class={"text-4xl py-20 text-center"}>{message}</p>
-        {:else}
-        <div class="lg:hover:blur-none">
-            <div class={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 border-solid border-2 border-sky-500 rounded-lg" +
-             " opacity-90 py-10 ` + (currentlySelected === "cyberpunk" ? `bg-yellow-400` : `bg-gray-800`)} style={"height: 60vh;"} id="log">
-            </div>
-            <div>
-                <form id="chatform" class="text-right px-12 py-2 lg:flex-none" autocomplete="off">
-                    <input type="text" class="input input-bordered w-full max-w-xs" id="text" />
-                    <input type="submit" class="px-1.5" id="send" />
-                </form>
-            </div>
-        </div>
-    {/if}
 
 </html>
